38a414623bd30602100cf0718ce2d970de74e1b0
Simeon Tran <siatran@ucsc.edu>
2022-03-21 21:56:20 -0700

    Got the application to open a new window each time the user tries to close a window.

--- gui-version/learn-gtk/practice/infinite-windows.c
+++ gui-version/learn-gtk/practice/infinite-windows.c
@@ -3,9 +3,19 @@
 /* ---- RESOURCES UTILIZED ----
  *
  * https://docs.gtk.org/gtk4/getting_started.html#building-applications
+ * 
  * https://wiki.gnome.org/HowDoI/GtkApplication
+ * 
  * https://www.cc.gatech.edu/data_files/public/doc/gtk/tutorial/gtk_tut-3.html
- * https://stackoverflow.com/questions/22384333/passing-additional-arguments-to-gtk-function
+ * 
+ * https://stackoverflow.com/questions/22384333/passing-additional
+ * -arguments-to-gtk-function
+ *
+ * https://stackoverflow.com/questions/1037414/when-the-deleteevent-is-called-by-a
+ * -gtk-window-how-can-i-prevent-the-window-fr
+ *
+ * https://docs.gtk.org/gtk4/class.Window.html#:~:text=A%20GtkWindow%20is%20a
+ * %20toplevel,%2C%20close%20it%2C%E2%80%A6).
  * 
  * */
 
@@ -13,6 +23,12 @@
 #define HEIGHT 500
 
 #define MESSAGE "Click me to open another window.\n"
+
+gboolean close_request(GtkWindow *window_to_close, gpointer user_data) {
+	g_print("A window has been closed.\n");
+	gtk_window_close(window_to_close);
+	return true;
+}
 
 /* Creates a new window with a button that opens another window
  * when clicked.
@@ -29,6 +45,9 @@ void another_window(GtkWidget *unused, gpointer auxillary) {
 	g_signal_connect(button_new_window, "clicked", G_CALLBACK(another_window), 
 					 current_application);
 	gtk_window_set_child(GTK_WINDOW(new_window), button_new_window);
+
+	g_signal_connect(new_window, "close_request", G_CALLBACK(another_window), 
+					 current_application);
 
 	gtk_window_present(GTK_WINDOW(new_window));
 	return;
