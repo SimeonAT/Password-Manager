1511226e5dc610bb00bb4fd351ca20d645c980ad
Simeon Tran <siatran@ucsc.edu>
2022-03-23 21:51:54 -0700

    Created a box to determine the size of a button.

--- gui-version/learn-gtk/practice/hello-there.c
+++ gui-version/learn-gtk/practice/hello-there.c
@@ -6,8 +6,6 @@
  * 
  * https://docs.gtk.org/gtk4/class.Label.html
  *
- * https://docs.gtk.org/gtk4/class.Label.html
- *
  * https://docs.gtk.org/Pango/pango_markup.html
  *
  * https://docs.gtk.org/gtk4/class.Button.html
@@ -16,6 +14,7 @@
 
 #define WIDTH 500
 #define HEIGHT 500
+#define PIXELS_BETWEEN 100
 
 #define MESSAGE "Hello There!"
 #define MESSAGE_LABEL "<span font='30'>Hello There!</span>"
@@ -44,20 +43,32 @@ void response_window(GtkApplication *current_app, gpointer app_pointer) {
  */
 void main_window(GtkApplication *current_application, 
 				 gpointer unused) {
+	
+	// Create the window itself.
 	GtkWidget *new_window = gtk_application_window_new(current_application);
 	gtk_window_set_title(GTK_WINDOW(new_window), MESSAGE);
 	gtk_window_set_default_size(GTK_WINDOW(new_window), WIDTH, HEIGHT);
 
+	// Create the message label to be displayed on the button.
 	GtkWidget *message_label = gtk_label_new(NULL);
 	gtk_label_set_markup(GTK_LABEL(message_label), MESSAGE_LABEL);
 
+	// Create the button with the specific message label. 
 	GtkWidget *button_new_window = gtk_button_new();
 	g_signal_connect_swapped(button_new_window, "clicked", 
 							 G_CALLBACK(response_window), 
 					 	 	 current_application);
-
-	gtk_window_set_child(GTK_WINDOW(new_window), button_new_window);
 	gtk_button_set_child(GTK_BUTTON(button_new_window), message_label);
+
+	// Create the box that will contain the button object.
+	GtkWidget *box_new_window = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 
+											PIXELS_BETWEEN);
+	gtk_widget_set_halign(box_new_window, GTK_ALIGN_CENTER);
+	gtk_widget_set_valign(box_new_window, GTK_ALIGN_CENTER);
+
+	// Relationships between box and button, and box and window.
+	gtk_window_set_child(GTK_WINDOW(new_window), box_new_window);
+	gtk_box_append(GTK_BOX(box_new_window), button_new_window);
 
 	gtk_window_present(GTK_WINDOW(new_window));
 	return;
